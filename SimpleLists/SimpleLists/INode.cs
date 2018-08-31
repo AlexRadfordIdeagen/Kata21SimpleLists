0fc231dea19020ccb24a49b1b1eaba910bfbec75
IDEAGENPLC\Alex.Radford <alex.radford@ideagenplc.com>
2018-08-31 10:24:23 +0100

    Added Inode and linkedlist classes

---
+++ SimpleLists/SimpleLists/INode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLists
+{
+    public class INode<T>
+    {
+        public INode<T> next;
+        public T data;
+    }
+}
---
+++ SimpleLists/SimpleLists/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLists
+{
+    public class LinkedList<T> where  T : INode<T>
+    {
+        private INode<T> head;
+    }
+}
